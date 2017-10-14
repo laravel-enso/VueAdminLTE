bc4465aa854366c7357b8babc7143c6c1c124c75
mishu <mishu@xtelecom.ro>
2017-10-14 18:17:17 +0300

    adds enso-update tag

--- src/VueAdminLTEServiceProvider.php
+++ src/VueAdminLTEServiceProvider.php
@@ -11,6 +11,10 @@ class VueAdminLTEServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/resources/assets/js' => resource_path('assets/js/vendor/laravel-enso'),
         ], 'vue-components');
+
+        $this->publishes([
+            __DIR__.'/resources/assets/js' => resource_path('assets/js/vendor/laravel-enso'),
+        ], 'enso-update');
     }
 
     public function register()
