65656e55ec71815c06e98bc187eff33ebfad3040
Mihai <mihai.ocneanu@gmail.com>
2017-12-07 13:34:49 +0200

    added addresses components

---
+++ src/resources/assets/js/components/addresses/AddressModalForm.vue
@@ -0,0 +1,112 @@
+<template>
+    <transition name="modal" >
+        <div class="contract-modal-mask flex-center-xy">
+            <div class="contract-modal-container">
+                <div class="col-md-12">
+                    <div class="row">
+                        <vue-form
+                            v-if="form"
+                            :data="form"
+                            :open="isOpen"
+                            :removable="true"
+                            ref="addressForm"
+                            v-on="$listeners">
+                        </vue-form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </transition>
+</template>
+
+<script>
+
+    export default {
+        props: {
+            id: {
+                default: null
+            },
+            type: {
+                type: String,
+                default: ""
+            },
+            address: {
+                type: Object,
+                default: null
+            },
+        },
+
+        computed: {
+
+        },
+
+        data() {
+            return {
+                errors: {},
+                labels: Store.labels,
+                isOpen: false,
+                form: null
+            };
+        },
+
+        methods: {
+            getEditForm() {
+                try {
+                    axios.get('/addresses/getEditForm/' + this.address.id).then(response => {
+                        this.form = response.data;
+                    }).then(()=> {
+                        this.isOpen = true; //box fix
+                    });
+                } catch(error) {
+                    this.reportEnsoException(error);
+                }
+            },
+            getCreateForm() {
+                try {
+                    const params = {addressable_id: this.id, addressable_type: this.type};
+                    axios.get('/addresses/getCreateForm', {params: params}).then(response => {
+                        this.form = response.data;
+                    }).then(()=> {
+                        this.isOpen = true; //box fix
+                    });
+                } catch(error) {
+                    this.reportEnsoException(error);
+                }
+            },
+        },
+        mounted: function () {
+            this.$nextTick(()=>{
+                this.address ? this.getEditForm() : this.getCreateForm();
+            });
+        }
+    }
+
+</script>
+
+<style>
+    .contract-modal-mask {
+        position: fixed;
+        z-index: 9998;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, .5);
+        transition: opacity .3s ease;
+    }
+
+    .contract-modal-container {
+        min-width: 250px;
+        min-height: 415px;
+        width:80%;
+        max-height: 90%;
+        overflow-y: auto;
+        margin: 0px auto;
+        padding: 5px 5px;
+        color: #3c3a3a;
+        background-color: #fff;
+        border-radius: 2px;
+        box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
+        transition: all .3s ease;
+    }
+</style>
